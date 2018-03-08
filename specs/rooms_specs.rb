1bf0abfd01baaf21f1b0e2385f5627d096c88111
Brenda Rios <riosbrenda@gmail.com>
2018-03-07 22:48:36 -0800

    Tests created for class Room.Will probably need to add or modify.

---
+++ specs/rooms_specs.rb
@@ -0,0 +1,31 @@
+require_relative 'spec_helper'
+require 'pry'
+
+describe 'Rooms class' do
+  before do
+
+  end
+
+  describe 'Initialize' do
+    it "Can be created" do
+
+    end
+
+  end
+
+  describe "Available rooms in date range " do
+
+    it "Returns list of available rooms if there are NO reservations" do
+
+    end
+
+    it "Returns empty array if no vacancies" do
+
+    end
+
+    it "Returns the right number of rooms available" do
+
+    end
+
+  end
+end
