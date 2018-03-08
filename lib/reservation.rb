f7e8471d76915e68b641ff601509d0c7782dd513
Brenda Rios <riosbrenda@gmail.com>
2018-03-07 22:22:47 -0800

    Modified some variables names

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,16 +1,16 @@
 module Hotel
 
   class Reservation #This class will calculate the total cost for a given reservation.
-    attr_reader :checkin, :checkout, :days_range
+    attr_reader :checkin, :checkout, :range_of_dates
 
     PRICE_PER_NIGHT = 200
     def initialize(checkin, checkout)
       @price_per_night = PRICE_PER_NIGHT
-      @range_of_dates = Hotel::RangeDates.new(checkin, checkout)
+      @range_of_dates = Hotel::DatesRange.new(checkin, checkout)
     end
 
     def cost_reservation
-      @price_per_night * @range_of_dates.stay_length
+      @price_per_night * @range_of_dates.amount_days
     end
 
   end #class
--- specs/booking_specs.rb
+++ specs/booking_specs.rb
@@ -12,7 +12,7 @@ describe 'Booking class' do
     end
 
     it "Create list of Reservation" do
-      @booking.list_reservations.must_equal []
+      @booking.reservations_list.must_equal []
     end
   end
 
@@ -34,15 +34,18 @@ describe 'Booking class' do
   describe "reservations_per_day method" do
     before do
       @date1 = Date.new(2018,02,20)
-      @date2 = Date.new(2018,03,23)
+      @date2 = Date.new(2018,02,24)
       @date3 = Date.new(2018,03,31)
       @date4 = Date.new(2018,04,04)
       @date5 = Date.new(2018,04,29)
+      @date6 = Date.new(2018,05,02)
+      @date6 = Date.new(2018,05,10)
       @booking = Hotel::Booking.new
+      @booking.add_reservation(@date1, @date2)
       @booking.add_reservation(@date1, @date3)
-      @booking.add_reservation(@date1, @date4)
-
-
+      @booking.add_reservation(@date2, @date3)
+      @booking.add_reservation(@date4, @date5)
+      @booking.add_reservation(@date5, @date6)
     end
 
     it 'Returns an array of reservations instances' do
@@ -54,7 +57,7 @@ describe 'Booking class' do
     end
 
     it 'Returns empty array if that day there is no reservations' do
-      @booking.reservations_per_day(@date2).must_equal []
+      @booking.reservations_per_day(@date6).must_equal []
     end
 
     it 'Returns all the reservations per day, when is not empty' do
