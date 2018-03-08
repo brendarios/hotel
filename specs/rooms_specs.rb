b1177e8740a82a38db88ec6f203e8bf7ea881c86
Brenda Rios <riosbrenda@gmail.com>
2018-03-07 23:47:34 -0800

    Update on the tests for class Rooms.

--- specs/rooms_specs.rb
+++ specs/rooms_specs.rb
@@ -1,14 +1,28 @@
-require_relative 'spec_helper'
+  require_relative 'spec_helper'
 require 'pry'
 
 describe 'Rooms class' do
   before do
-
+    @room = Hotel::Room.new(1, 200)
   end
 
   describe 'Initialize' do
-    it "Can be created" do
+    it "creates an instance" do
+      @room.must_be_instance_of Hotel::Rooms
+    end
 
+    it "raises an error if the room number is not valid" do
+      proc { room = Hotel::Room.new(30, 200) }.must_raise ArgumentError
+    end
+
+    it "responds to the room number" do
+      @room.number.must_be_kind_of Integer
+      @room.must_respond_to :number
+    end
+
+    it "access the status of the room" do
+      @room.number.must_be_kind_of Symbol
+      @room.must_equal :AVAILABLE
     end
 
   end
