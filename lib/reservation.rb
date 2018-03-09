7fa700a3ef7088c5a538dc72a0675e70a8b4dcba
Brenda Rios <riosbrenda@gmail.com>
2018-03-09 10:45:07 -0800

    Add parameter room number to Reservation class

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,17 +1,18 @@
 module Hotel
 
   class Reservation #This class will calculate the total cost for a given reservation.
-    attr_reader :checkin, :checkout, :range_of_dates
+    attr_reader :checkin, :checkout, :range_of_dates, :room_number
 
-    PRICE_PER_NIGHT = 200
+
     def initialize(checkin, checkout)
       @price_per_night = PRICE_PER_NIGHT
       @range_of_dates = Hotel::DatesRange.new(checkin, checkout)
+      @room_number = room_number
     end
 
     def cost_reservation
       @price_per_night * @range_of_dates.amount_days
     end
 
-  end #class
-end #module
+  end
+end
