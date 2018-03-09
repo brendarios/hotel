968b421d9226047147eadf13173cbeb97951dd7d
Brenda Rios <riosbrenda@gmail.com>
2018-03-09 12:14:05 -0800

    Refactored Reservation class, I was missing to initialize the room_number variable.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,10 +1,10 @@
 module Hotel
 
-  class Reservation #This class will calculate the total cost for a given reservation.
+  class Reservation
     attr_reader :checkin, :checkout, :range_of_dates, :room_number
 
 
-    def initialize(checkin, checkout)
+    def initialize(checkin, checkout, room_number)
       @price_per_night = PRICE_PER_NIGHT
       @range_of_dates = Hotel::DatesRange.new(checkin, checkout)
       @room_number = room_number
