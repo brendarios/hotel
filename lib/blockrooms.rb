f6334bf6492ccfdb7b0d507e5e25c051d8c72460
Brenda Rios <riosbrenda@gmail.com>
2018-03-09 17:36:59 -0800

    Implementation of methods for class BlockRooms.

--- lib/blockrooms.rb
+++ lib/blockrooms.rb
@@ -1,24 +1,24 @@
 module Hotel
   class BlockRooms
 
-    def initialize(checkin, checkout, rooms_set, rate)
+    def initialize(checkin, checkout, blocked_rooms, rate)
       @price_per_night = rate
       @days_range = Hotel::DatesRange.new(checkin, checkout)
-      @rooms_set = rooms_set
+      @blocked_rooms = blocked_rooms
     end
 
     def available_room_in_block?
-
+      @blocked_rooms.length > 0 ? true : false
     end
 
     def reservation_block_room
-
-        raise ArgumentError.new("Sorry, no more rooms available in the blocked rooms for this event")
-
+      raise ArgumentError.new("Unavailable blocked rooms") if !available_room_in_block?
+      @blocked_rooms.delete_at(0)
+      return @blocked_rooms
     end
 
     def total_cost_block_room
-      
+      @price_per_night * @days_range.amount_days
     end
 
   end
