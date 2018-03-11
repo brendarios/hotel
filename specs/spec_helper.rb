05017e5a02d9bd7e8e86e1d25e25cd249d6dcea5
Brenda Rios <riosbrenda@gmail.com>
2018-03-11 11:26:31 -0700

    For design purposes I decided to don't use Rooms class therefore, the spec_helper file was upated.

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -6,7 +6,6 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
 
-
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 # Require_relative your lib files here!
 require_relative '../lib/booking'
